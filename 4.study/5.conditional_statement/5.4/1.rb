637c0b00e09699c18a39a3f8aafeb6479a803dbd
stgim <stgim@mobigen.com>
2021-04-28 12:58:08 +0900

    5-4 수업 내용 추가

---
+++ 4.study/5.conditional_statement/5.4/1.rb
@@ -0,0 +1,12 @@
+input = 22
+real_gst = 22
+real_kst = "ab"
+if real_gst == input
+    puts("Hello gst")
+else
+    if real_kst == input
+        puts("Hello kst")
+        else
+        puts("Who are you?")
+    end
+end
---
+++ 4.study/5.conditional_statement/5.4/2.py
@@ -0,0 +1,9 @@
+input = 22
+real_gst = 22
+real_kst = "ab"
+if real_gst == input:
+    print("Hello gst")
+elif real_kst == input:
+    print("Hello kst")
+else:
+    print("Who are you?")
