196adf45bf7b733f3632226911438c417916a902
stgim <stgim@mobigen.com>
2021-04-30 09:51:34 +0900

    6.input_output 수업추가

---
+++ 4.study/6.input_output/6-1/1.rb
@@ -0,0 +1,12 @@
+# 로그인 애플리케이션에 입력 기능 추가하기
+puts("id를 입력해 주세요.")
+input = gets.chomp()
+real_gst = "22"
+real_kst = "ab"
+if real_gst == input
+    puts("Hello gst")
+elsif real_kst == input
+    puts("Hello kst")
+else
+    puts("Who are you?")
+end
