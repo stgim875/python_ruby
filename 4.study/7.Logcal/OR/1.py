594e866d5b9b44e2aa3cc1f37360e54583943ecf
stgim <stgim@mobigen.com>
2021-05-03 10:16:51 +0900

    수업 업데이트

---
+++ 4.study/7.Logcal/OR/1.py
@@ -0,0 +1,8 @@
+# OR 논리 연산자
+in_str = input("아이디를 입력해주세요.\n")
+real_gst = "gst"
+real_kst = "kst"
+if real_gst == in_str or real_kst == in_str:
+    print("Hello!")
+else:
+    print("Who are you?")
---
+++ 4.study/7.Logcal/OR/1.rb
@@ -0,0 +1,10 @@
+# OR 논리 연산자
+puts("아이디를 입력해주세요.\n")
+input = gets.chomp()
+real_id = "gst"
+real_id = "11"
+if real_id == input_id or real_pwd == input_pwd
+    puts("Hello!")
+else
+    puts("Who are you?")
+end
