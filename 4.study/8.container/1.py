86e23480449f9298a0899f8e2da8ac1b4ed7b576
stgim <stgim@mobigen.com>
2021-05-04 17:13:51 +0900

    8.container 수업 업데이트

--- 4.study/8.container/1.py
+++ 4.study/8.container/1.py
@@ -2,11 +2,15 @@
 # 하나의 값을 담을 수 있는 그릇
 # 여려 개를 담을 수 없을까?
 
-# List
-# 기본 사용법
 print(type('gst'))
 name = 'gst'
 print(name)
+
+# List
+# 기본 사용법
+# 숫자를 인덱스 또는 색인
+# 리스트에 담겨 있는 값들을 원소 또는 Element라고 한다.
+
 print(type(['gst', 'kim', 'kst']))
 names = ['gst', 'kim', 'kst']
 print(names)
@@ -16,7 +20,3 @@ print(names[2])
 gst = ['program', 'gunpo', 42, True]
 gst[1] = 'kimpo'
 print(gst)
-
-
-# 숫자를 인덱스 또는 색인
-# 리스트에 담겨 있는 값들을 원소 또는 Element라고 한다.
--- 4.study/8.container/review/1.rb
+++ 4.study/8.container/review/1.rb
@@ -7,7 +7,7 @@ puts(name.class)
 puts(name)
 
 # Array
-# 기본 사용법
+# 기본 사용법 복습
 # 숫자를 인덱스 또는 색인
 # 리스트에 담겨 있는 값들을 원소 또는 Element라고 한다.
 
