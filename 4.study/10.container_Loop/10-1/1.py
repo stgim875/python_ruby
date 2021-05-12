a7247473967550150a056cb73ae6308696d8f712
stgim <stgim@mobigen.com>
2021-05-12 10:28:43 +0900

    수업 재업데이트

---
+++ 4.study/10.container_Loop/10-1/1.py
@@ -0,0 +1,4 @@
+# 달콤한 for문의 등장
+members = ['gst', 'kst', 'kim']
+for member in members:
+    print(member)
---
+++ 4.study/9.Loop/9-1/1.py
@@ -0,0 +1,5 @@
+# while 알아보기
+# while 뒤에 따라 올수 있는 값은 True 또는 False 가 온다, 즉 Boolean이 온다.
+while False:
+    print('Hello World')
+print('After while')
