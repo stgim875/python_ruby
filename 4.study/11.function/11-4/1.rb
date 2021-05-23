395907e85e81d00ee45a45caa2568f3bf31d0d05
stgim <stgim@mobigen.com>
2021-05-24 08:47:30 +0900

    11-3, 11-4 수업 추가

---
+++ 4.study/11.function/11-4/1.rb
@@ -0,0 +1,5 @@
+# 여러개의 입력값
+def make_string(str, num)
+    return str*num
+end
+puts(make_string('abc', 100))
---
+++ 4.study/11.function/11-4/review/1.rb
@@ -0,0 +1,6 @@
+# 여러개의 입력값 복습
+def make_string(str, num)
+    return str*num
+end
+
+puts(make_string('b', 3))
