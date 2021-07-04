aa859980437381b36cdeb6a2aaf4056cc0f5fe1d
stgim <stgim@mobigen.com>
2021-07-05 08:29:29 +0900

    2.string/1.new 하위 수업 추가

---
+++ 4.study/2.string/1.new/4.special characters/1.rb
@@ -0,0 +1,6 @@
+puts("seong's \"tutorial\"") # \(escaping)는 탈출 시키다.
+puts("\\")
+puts("Hello\nworld") # \n은 줄바꿈의 임무를 가지고 있음
+puts("Hello\t\tworld")
+puts("\a")
+puts('Hello\nworld') # 루비는 ''(작은 따옴표)와 ""(큰 따옴표)와 기능 차이가 있다.
