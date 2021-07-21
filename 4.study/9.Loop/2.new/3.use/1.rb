742da35fc31d057b74297e55ccacc88e58096a9d
stgim <stgim@mobigen.com>
2021-07-22 08:27:37 +0900

    9.Loop/2.new 하위 수업 추가

---
+++ 4.study/9.Loop/2.new/3.use/1.rb
@@ -0,0 +1,7 @@
+# 활용
+
+i = 0
+while i < 10 do  # 비교와 블리언이 들어옴(==, <, >, False, True)
+    puts('puts("Hello world '+(i*9).to_s()+'")')
+    i = i + 1
+end
---
+++ 4.study/9.Loop/2.new/4.condition and loop combination/1.rb
@@ -0,0 +1,9 @@
+# 조건문과 반복문의 합체
+i = 0
+while i < 10 do
+    if i != 4
+        puts(i)
+    end
+    i = i + 1
+end
+print('after while')
