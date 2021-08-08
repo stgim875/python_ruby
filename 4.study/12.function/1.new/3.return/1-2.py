d2754bee5185ab54c0483b57f8c29f80b93ebe8b
stgim <stgim@mobigen.com>
2021-08-09 08:33:57 +0900

    12.function/2.new 하위 수업 추가

---
+++ 4.study/12.function/1.new/3.return/1-2.py
@@ -0,0 +1,29 @@
+#리턴값
+
+# print(len('aa'))
+# print('a'*3)
+# print(1+1)
+# print(2)
+
+
+def a3():
+    return 'aaa'
+print(a3())
+
+def aa():
+    return 'aa'
+print(aa())
+
+def ab():
+    return 'ab'
+print(ab())
+
+def a3():
+    a = 'aaa'
+    return a
+print(a3())
+
+def a3():
+    print('before')
+    return 'aaa'
+    print('after')
