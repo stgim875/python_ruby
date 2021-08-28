6c568d80215106d0c2c44d5f0fc9e034adbce595
stgim875 <stgim875@gmail.com>
2021-08-28 14:53:39 +0900

    긴급 업데이트

--- 4.study/12.function/1.new/array_block/1-2.rb
+++ 4.study/12.function/1.new/array_block/1-2.rb
@@ -1,10 +1,17 @@
 # 배열과 블록(2021-08-18)
 
 # a , b , c 소문자로 출력
+<<<<<<< Updated upstream
 # ['A', 'B', 'C'].each(){|i| puts i.downcase}
 
 arr = ['A', 'B', 'C']
 arr.each(){|i| puts i} # each는 하나씩 꺼낸다는 뜻
+=======
+['A', 'B', 'C'].each(){|i| puts i.downcase}
+
+arry = ['A', 'B', 'C']
+arr.each(){|i| puts i.} # each는 하나씩 꺼낸다는 뜻
+>>>>>>> Stashed changes
 
 for value in arr
     puts value
