1895930c1bb1df5225d3f11119d64c400f83bd0a
stgim <stgim@mobigen.com>
2021-09-08 08:52:13 +0900

    15.object_creation 하위 수업 추가

---
+++ 4.study/15.object_creation/5. instance variable_method/1.rb
@@ -0,0 +1,32 @@
+# 인스턴스 변수와 메소드
+# 계산기를 만들어 보기
+
+# 하나의 클래스가 내부적으로 서로 다른 데이터를 가지고 있음
+# 그 데이터에 따라서 메소드를 실행한 결과가 각자 다를 것이다. 실행한적은 없지만..
+
+# 생성자 : constructor
+
+# Cal.new 인스턴스를 실행할 때 제일 먼저 initialize가 실행 되도록 약속되어 있음
+class Cal # 객체는 늘 대문자로 시작
+    def initialize(v1, v2)
+    @v1 = v1 # @는 인스턴스 변수
+    @v2 = v2
+    end
+    def add()
+        return @v1+@v2
+    end
+    def subtract()
+        return @v1-@v2
+    end
+end
+
+c1 = Cal.new(10, 10)
+# Cal을 실행하면 Cal이라는 calss를 복제한 인스턴스가 생성된다.
+# 생성되는 과정에서 이렇게 두개의 입력값이 인스턴스의 데이터로 설정
+
+p c1.add()
+p c1.subtract()
+
+c2 = Cal.new(30, 20)
+p c2.add()
+p c2.subtract()
