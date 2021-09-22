db6479b6f593380e2d3231dc2947b2f36804a067
stgim <stgim@mobigen.com>
2021-09-23 08:51:54 +0900

    15.object_creation/1.new 하위 수업 추가

---
+++ 4.study/15.object_creation/2.object_use/class2.py
@@ -0,0 +1,9 @@
+# 객체 사용
+
+c1 = Cal(10, 10)
+print(c1.add())
+print(c1.subtract())
+
+c2 = Cal(30, 20)
+print(c1.add())
+print(c2.subtract())
---
+++ 4.study/15.object_creation/2.object_use/class2.rb
@@ -0,0 +1,9 @@
+# 객체 사용 (2021-09-23)
+
+c1 = Cal.new(10, 10)
+p c1.add()
+p c1.subtract()
+
+c2 = Cal.new(30, 20)
+p c2.add()
+p c2.subtract()
