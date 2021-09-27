c9398076c67783dcc8d8714731be89566b5f18a4
stgim <stgim@mobigen.com>
2021-09-27 09:06:50 +0900

    15.object_creation/4.constructor 수업 추가

---
+++ 4.study/15.object_creation/4. constructor/1-1.py
@@ -0,0 +1,14 @@
+# 생성자 (2021-09-27)
+
+class Cal(object):
+    def __init__(self, v1, v2):
+        print(v1, v2)
+
+
+c1 = Cal(10, 10)
+# print(c1.add())
+# print(c1.subtract())
+
+c2 = Cal(30, 20)
+# print(c1.add())
+# print(c2.subtract())
