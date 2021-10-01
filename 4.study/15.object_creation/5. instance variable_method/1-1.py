c482886fa2b64fe1710973dcbb7f4508c03e4533
stgim <stgim@mobigen.com>
2021-10-01 17:43:22 +0900

    업데이트

--- 4.study/15.object_creation/5. instance variable_method/1-1.py
+++ 4.study/15.object_creation/5. instance variable_method/1-1.py
@@ -1,7 +1,6 @@
 class Cal(object):
-
     def __init__(self, v1, v2):
-        self.v1 = v1
+        self.v1 = v1  # self가 인스턴스 변수임
         self.v2 = v2
 
     def add(self):
@@ -13,8 +12,8 @@ class Cal(object):
 
 c1 = Cal(10, 10)
 print(c1.add())
-print(c1.subtract()
+print(c1.subtract())
 
-c2=Cal(30, 20)
+c2 = Cal(30, 20)
 print(c2.add())
 print(c2.subtract())
