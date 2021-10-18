cc0c498f10e5d4e75b24158ce8a2d5d34732c8c0
stgim <stgim@mobigen.com>
2021-10-19 08:46:57 +0900

    4.class_variable(Ruby) 수업 추가

---
+++ 4.study/18.class_member/5.class_variable(Python)/1.py
@@ -0,0 +1,19 @@
+# 클래스 변수(2021-10-19)
+
+class Cs:
+    count = 0
+
+    def __init__(self):
+        Cs.count = Cs.count + 1
+
+    @classmethod
+    def getCount(cls):
+        print(cls)
+        return Cs.count
+
+
+i1 = Cs()
+i2 = Cs()
+i3 = Cs()
+i4 = Cs()
+print(Cs.getCount())
