70aed6292b145e19c413e9db42a906aca9ea0297
stgim <stgim@mobigen.com>
2021-10-20 11:19:46 +0900

    17.inheritance 코드 업데이트

--- 4.study/17.inheritance/3.inheritance_application/1.rb
+++ 4.study/17.inheritance/3.inheritance_application/1.rb
@@ -1,46 +1,41 @@
 # 상속의 응용(2021-10-14)
 
 class Cal
-    def initialize(v1, v2)
-        @v1 = v1 #@가 있는 것은 인스턴스 변수이다.
-        @v2 = v2
-    end
-    def add()
-        return @v1+@v2
-    end
-    def subtract()
-        return @v1-@v2
-    end
-    # method = function이다.
-    def setV1(v)
-        # 만약 v가 숫자라면?:
-        if v.is_a?(Integer)
-        @v1 = v
-        end
-    end
-    def getV1()
-        return @v1
+ attr_reader :v1, :v2
+ attr_writer :v1
+ def initialize(v1,v2)
+    @v1 = v1
+    @v2 = v2
+end
+def add()
+    return @v1+@v2
+end
+def subtract()
+    return @v1-@v2
+end
+def setV1(v)
+    if v.is_a?(Integer)
+      @v1 = v
     end
 end
-
-class CalMultiplay < Cal
-    def multiplay()
-        return @v1*@v2
-    end
+def getV1()
+    return @v1
 end
-
-class CalDivide < CalMultiplay
-    def divide()
-        return @v1/@v2
-    end
 end
-
-
-c1 = CalMultiplay.new(10, 10)
+class CalMultiply < Cal
+def multiply()
+    return @v1*@v2
+  end
+end
+class CalDivide < CalMultiply
+def divide()
+    return @v1/@v2
+end
+end
+c1 = CalMultiply.new(10,10)
 p c1.add()
-p c1.multiplay()
-
+p c1.multiply()
 c2 = CalDivide.new(20, 10)
 p c2, c2.add()
-p c2, c2.multiplay()
+p c2, c2.multiply()
 p c2, c2.divide()
