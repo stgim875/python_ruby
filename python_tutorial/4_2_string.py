6fb8126652ea652ebc9fe127bc16ccef927fc0f7
stgim <stgim@mobigen.com>
2021-11-02 08:42:23 +0900

    python basic 수업 추가

---
+++ python_tutorial/4_2_string.py
@@ -0,0 +1,15 @@
+# <데이터 타입>
+# 문자형 데이터 타입(2021-11-02)
+
+print('Hello world')
+print("Hello worold")
+print('''
+Hello 
+world
+''')
+
+print('1'+'1', '1'+'1')
+print('Hello world'*1000)
+print("len('Hello world'*1000)", len('Hello world'*1000))
+print("('Hello world'.replace('world', 'unive'))",
+      'Hello world'.replace('world', 'univers'))
