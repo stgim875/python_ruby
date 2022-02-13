3acab7a481ee648eb607f239a559b55236e64910
stgim <stgim@mobigen.com>
2022-02-14 08:55:28 +0900

    12.container 수업 추가

---
+++ 5.study/12.container/1.py
@@ -0,0 +1,11 @@
+print(type('gst'))
+name = 'gst'
+print(name)
+
+print(type(['gst', 'kst', 'kim']))
+names = ['gst', 'kst', 'kim']
+print(names)
+print(names[0])
+gst = ['program', 'gunpo', 43, False]
+gst[1] = 'kimpo'
+print(gst)
