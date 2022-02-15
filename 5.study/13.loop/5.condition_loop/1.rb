f2fcf080b4e786c38d7653352ccf39f9aea3af1f
stgim <stgim@mobigen.com>
2022-02-16 08:42:01 +0900

    5.조건문과 반복문 합체

---
+++ 5.study/13.loop/5.condition_loop/1.rb
@@ -0,0 +1,12 @@
+# 2022년 2월 16일
+# 조건문과 반복문의 합체
+
+i = 0
+while i < 10 do
+    if i ==4
+        break
+    end
+    puts(i)
+    i = i + 1
+end
+print('after while')
---
+++ 5.study/13.loop/5.condition_loop/2.py
@@ -0,0 +1,10 @@
+# 2022년 2월 16일
+# 조건문과 반복문의 합체
+
+i = 0
+while i < 10:
+    if i ==4:
+        break
+    print(i)
+    i = i + 1
+print('after while')
