c029f22cbea5ce2f7fcce682850b4de51e8ba8df
stgim <stgim@mobigen.com>
2022-02-15 11:47:37 +0900

    수업 업데이트

---
+++ 5.study/13.loop/4.use/1.py
@@ -0,0 +1,8 @@
+# 2022년 2월 15일
+# 활용
+# str명령어로 문자열로 만들 수 있음
+
+i = 0
+while i < 10:
+    print('print("Hello world '+str(i*9)+'")')
+    i = i + 1
---
+++ 5.study/13.loop/4.use/1.rb
@@ -0,0 +1,9 @@
+# 2022년 2월 15일
+# 활용
+# .to_s()로 문자열로 만들 수 있음
+
+i = 0
+while i < 10 do
+    puts('print("Hello world '+(i*9).to_s()+'")')
+    i = i + 1
+end
