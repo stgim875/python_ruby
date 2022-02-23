01682bbe7df7b4ac443b59f63892909751505537
stgim <stgim@mobigen.com>
2022-02-23 10:23:44 +0900

    업데이트

---
+++ 5.study/16.function/4.return_input/1.py
@@ -0,0 +1,14 @@
+# 2022년 2월 23일
+# return_input
+
+# def a3():
+#     return 'aaa'
+# print(a3())
+
+# def a4():
+#     return 'aaaa'
+# print(a4())
+
+def a(num):
+    return 'a' * num
+print(a(100))
---
+++ 5.study/16.function/4.return_input/1.rb
@@ -0,0 +1,7 @@
+# 2022년 2월 23일
+# return_input
+
+def a(num)
+    return 'a' * num
+end
+puts(a(100))
