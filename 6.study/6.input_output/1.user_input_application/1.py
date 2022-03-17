b3245683066b1355fdf9a1e610a8d6b011dfdb6f
stgim <stgim@mobigen.com>
2022-03-17 15:59:43 +0900

    삭제

--- 6.study/6.input_output/1.user_input_application/1.py
+++
@@ -1,5 +0,0 @@
-# 2022년 3월 17일
-# input_output
-
-in_str = input("입력해주세요.\n")
-print(in_str.upper()+" World!")
--- 6.study/6.input_output/1.user_input_application/1.rb
+++
@@ -1,6 +0,0 @@
-# 2022년 3월 17일
-# input_output
-
-puts("입력해주세요.\n")
-in_str = gets.chomp()
-puts(in_str.upcase()+" World!")
