b0839c735bcf9c750cf39417eb886888985da20a
stgim <stgim@mobigen.com>
2022-03-17 15:57:51 +0900

    업데이트

---
+++ 6.study/6.input_output/1.user_input_application/1.py
@@ -0,0 +1,5 @@
+# 2022년 3월 17일
+# input_output
+
+in_str = input("입력해주세요.\n")
+print(in_str.upper()+" World!")
---
+++ 6.study/6.input_output/1.user_input_application/1.rb
@@ -0,0 +1,6 @@
+# 2022년 3월 17일
+# input_output
+
+puts("입력해주세요.\n")
+in_str = gets.chomp()
+puts(in_str.upcase()+" World!")
